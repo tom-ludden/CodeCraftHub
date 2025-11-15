bd8702647a59375b9ac3948331bbdb5781cd24b4
tom-ludden <tomerin79@gmail.com>
2025-11-15 11:16:53 -0500

    q

--- src/utils/errorhandler.js
+++
@@ -1,6 +0,0 @@
-const logger = require('./logger');
-const errorHandler = (err, req, res, next) => {
-    logger.error(err);
-    res.status(500).json({ error: 'Something went wrong.' });
-};
-module.exports = errorHandler;
